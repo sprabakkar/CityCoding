92b65d9ca75438abe6f1e4ed7bb021abb728f6e3
sprabakkar <sprabakkar@gmail.com>
2024-08-23 21:43:22 -0400

    file updated

--- src/com/city/coding/TestClass.java
+++ src/com/city/coding/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String name = "John";
 		System.out.println("Hello! how are you!");
 	}
 
